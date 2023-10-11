e28837aae9ed33bee50135d7efbe7e7e47da1411
unknown <sophialhales@gmail.com>
2023-10-11 11:05:29 -0500

    ruleset entity class done?

--- src/main/java/com/accesspoint/rulesengine/Ruleset.java
+++ src/main/java/com/accesspoint/rulesengine/Ruleset.java
@@ -5,9 +5,7 @@ import java.util.Objects;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 
