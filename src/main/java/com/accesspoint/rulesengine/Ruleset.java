b77b270b43315beba582d7573e90ee42df52c191
unknown <sophialhales@gmail.com>
2023-10-11 10:58:46 -0500

    ruleset entity class in progress

---
+++ src/main/java/com/accesspoint/rulesengine/Ruleset.java
@@ -0,0 +1,46 @@
+package com.accesspoint.rulesengine;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+public class Ruleset {
+
+    @Getter @Setter private @Id @GeneratedValue Long id;
+
+    @Getter @Setter private String name;
+
+    Ruleset() {}
+
+    Ruleset(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object rs) {
+
+        if (this == rs)
+            return true;
+        if (!(rs instanceof Ruleset))
+            return false;
+        Ruleset ruleset = (Ruleset) rs;
+        return Objects.equals(this.id, ruleset.id) && Objects.equals(this.name, ruleset.name);
+    }
+
+    @Override
+    public int hashCode() { return Objects.hash(this.id, this.name); }
+
+    @Override
+    public String toString() {
+        return "Ruleset{" + "id=" + this.id + ", name=" + this.name;
+    }
+}
