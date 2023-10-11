0a2c1b41e0d20a8cfc4b0d64b54416f36668e182
ColeNelson806 <colenelson806@outlook.com>
2023-10-11 13:41:25 -0500

    Added sql folder with ruleset table creation code. Added creation_date field.

--- src/main/java/com/accesspoint/rulesengine/Ruleset.java
+++ src/main/java/com/accesspoint/rulesengine/Ruleset.java
@@ -1,13 +1,12 @@
 package com.accesspoint.rulesengine;
 
+import java.sql.Timestamp;
 import java.util.Objects;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-
+import org.hibernate.annotations.CreationTimestamp;
 import lombok.Getter;
 import lombok.Setter;
-
 
 @Entity
 public class Ruleset {
@@ -15,6 +14,9 @@ public class Ruleset {
     @Getter @Setter private @Id Long id;
 
     @Getter @Setter private String name;
+
+    @CreationTimestamp
+    @Getter @Setter private Timestamp creation_date;
 
     Ruleset(String name) {
         this.name = name;
@@ -28,14 +30,18 @@ public class Ruleset {
         if (!(rs instanceof Ruleset))
             return false;
         Ruleset ruleset = (Ruleset) rs;
-        return Objects.equals(this.id, ruleset.id) && Objects.equals(this.name, ruleset.name);
+        return Objects.equals(this.id, ruleset.id)
+                && Objects.equals(this.name, ruleset.name)
+                && Objects.equals(this.creation_date, ruleset.creation_date);
     }
 
     @Override
-    public int hashCode() { return Objects.hash(this.id, this.name); }
+    public int hashCode() {
+        return Objects.hash(this.id, this.name, this.creation_date);
+    }
 
     @Override
     public String toString() {
-        return "Ruleset{" + "id=" + this.id + ", name=" + this.name;
+        return "Ruleset{" + "id=" + this.id + ", name=" + this.name + ", creation date=" + this.creation_date;
     }
 }
