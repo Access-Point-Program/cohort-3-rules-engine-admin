1f2f6f970bac3b755c89faaa839cb5a561fcfb05
ColeNelson806 <colenelson806@outlook.com>
2023-11-02 10:46:03 -0500

    Forgot to create new branch, moving changes to new branch. Removed react and created new angular project, changed phases of install and build in pom, temp commenting out of backend modelassemblers

--- src/main/java/com/accesspoint/rulesengine/modelassembler/ConditionModelAssembler.java
+++ src/main/java/com/accesspoint/rulesengine/modelassembler/ConditionModelAssembler.java
@@ -8,16 +8,16 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
-@Component
-public class ConditionModelAssembler implements RepresentationModelAssembler<Condition, EntityModel<Condition>>{
-
-    @Override
-    public EntityModel<Condition> toModel(Condition condition) {
-
-        EntityModel<Condition> conditionModel = EntityModel.of(condition,
-                linkTo(methodOn(ConditionController.class).one(condition.getId())).withSelfRel(),
-                linkTo(methodOn(ConditionController.class).all()).withRel("conditions"));
-
-        return conditionModel;
-    }
-}
+//@Component
+//public class ConditionModelAssembler implements RepresentationModelAssembler<Condition, EntityModel<Condition>>{
+//
+//    @Override
+//    public EntityModel<Condition> toModel(Condition condition) {
+//
+//        EntityModel<Condition> conditionModel = EntityModel.of(condition,
+//                linkTo(methodOn(ConditionController.class).one(condition.getId())).withSelfRel(),
+//                linkTo(methodOn(ConditionController.class).all()).withRel("conditions"));
+//
+//        return conditionModel;
+//    }
+//}
--- src/main/java/com/accesspoint/rulesengine/modelassembler/RuleModelAssembler.java
+++ src/main/java/com/accesspoint/rulesengine/modelassembler/RuleModelAssembler.java
@@ -7,16 +7,16 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
-@Component
-public class RuleModelAssembler implements RepresentationModelAssembler<Rule, EntityModel<Rule>>{
-
-    @Override
-    public EntityModel<Rule> toModel(Rule rule) {
-
-        EntityModel<Rule> ruleModel = EntityModel.of(rule,
-                linkTo(methodOn(RuleController.class).one(rule.getId())).withSelfRel(),
-                linkTo(methodOn(RuleController.class).all()).withRel("rules"));
-
-        return ruleModel;
-    }
-}
+//@Component
+//public class RuleModelAssembler implements RepresentationModelAssembler<Rule, EntityModel<Rule>>{
+//
+//    @Override
+//    public EntityModel<Rule> toModel(Rule rule) {
+//
+//        EntityModel<Rule> ruleModel = EntityModel.of(rule,
+//                linkTo(methodOn(RuleController.class).one(rule.getId())).withSelfRel(),
+//                linkTo(methodOn(RuleController.class).all()).withRel("rules"));
+//
+//        return ruleModel;
+//    }
+//}
