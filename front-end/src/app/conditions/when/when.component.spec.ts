9d7daaecd04ff55a44d9e28ba4aba31cada49b8e
ColeNelson806 <colenelson806@outlook.com>
2023-11-07 14:31:55 -0600

    Finished dropdown option tests

--- front-end/src/app/conditions/when/when.component.spec.ts
+++ front-end/src/app/conditions/when/when.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { WhenComponent } from './when.component';
 
 describe('WhenComponent', () => {
@@ -20,26 +19,29 @@ describe('WhenComponent', () => {
   });
 
   describe('dropdown should contain the correct enums', () => {
-    const compiled = fixture.nativeElement as HTMLElement;
     it('First dropdown item should be \"FRONT\"', () => {
+      const compiled = fixture.nativeElement as HTMLElement;
       expect(compiled.querySelectorAll('.whenDropdownOptions').item(0).textContent).toContain("FRONT");
     });
 
     it('Second dropdown item should be \"RIGHT\"', () => {
+      const compiled = fixture.nativeElement as HTMLElement;
       expect(compiled.querySelectorAll('.whenDropdownOptions').item(1).textContent).toContain("RIGHT");
     });
 
     it('Third dropdown item should be \"LEFT\"', () => {
+      const compiled = fixture.nativeElement as HTMLElement;
       expect(compiled.querySelectorAll('.whenDropdownOptions').item(2).textContent).toContain("LEFT");
     });
 
     it('Fourth dropdown item should be \"BEHIND\"', () => {
+      const compiled = fixture.nativeElement as HTMLElement;
       expect(compiled.querySelectorAll('.whenDropdownOptions').item(3).textContent).toContain("BEHIND");
     });
 
-    it('Fifth dropdown item doesnt exist, so returns null', () => {
-      const typeError = new TypeError('Cannot read properties of undefined (reading \'nativeElement\')');
-      expect( () => {compiled.querySelectorAll('.whenDropdownOptions').item(4)}).toThrow(typeError);
-      });
+    it('Dropdown only contains 4 options', () => {
+      const compiled = fixture.nativeElement as HTMLElement;
+      expect(compiled.querySelectorAll('.whenDropdownOptions').length).toEqual(4);
+    });
   });
 });
