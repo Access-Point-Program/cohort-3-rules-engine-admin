528986c824e8c1daa5a987aabdc5c6495ecd36ec
Zach Duncan <izachary.duncan@gmail.com>
2023-11-07 10:19:29 -0600

    Added facts to dropdown

--- front-end/src/app/conditions/when/when.component.ts
+++ front-end/src/app/conditions/when/when.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
+
 @Component({
   selector: 'when-condition-selector',
   templateUrl: './when.component.html',
   styleUrls: ['./when.component.css']
 })
 export class WhenComponent {
-  constructor(private whenConditionDropDown: NgbDropdownModule) {}
+
+
+  // constructor(public factType: FactType) {}
+
+  factType: Array<string> = [
+    "FRONT",
+    "RIGHT",
+    "LEFT",
+    "BEHIND"
+  ];
+
+  returnedValue: string = '';
+
+  public setReturnedValue(returnedValue: string): void {
+    this.returnedValue = returnedValue;
+  };
+
 
   public checkIfDropdownOpen(isOpen: boolean): void {
     isOpen ? console.log("True") : console.log("False");
