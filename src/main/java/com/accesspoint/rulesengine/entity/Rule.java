390423f378bbe6982d1fc6fbda2bd9c6cabf3ed0
unknown <sophialhales@gmail.com>
2023-11-08 09:19:00 -0600

    fixed ruleset id naming

--- src/main/java/com/accesspoint/rulesengine/entity/Rule.java
+++ src/main/java/com/accesspoint/rulesengine/entity/Rule.java
@@ -18,7 +18,7 @@ public class Rule {
 
     @ManyToOne
     @JoinColumn(name = "ruleset_id")
-    @Getter @Setter private Ruleset ruleset_id;
+    @Getter @Setter private Ruleset ruleset;
 
     @Getter @Setter private double priority;
 
@@ -26,9 +26,9 @@ public class Rule {
 
     Rule() {}
 
-    public Rule(Long id, Ruleset ruleset_id, double priority, EventType event_type) {
+    public Rule(Long id, Ruleset ruleset, double priority, EventType event_type) {
         this.id = id;
-        this.ruleset_id = ruleset_id;
+        this.ruleset = ruleset;
         this.priority = priority;
         this.event_type = event_type;
         // if stuff breaks, add secondary key here
@@ -43,18 +43,18 @@ public class Rule {
             return false;
         Rule rule = (Rule) r;
         return Objects.equals(this.id, rule.id)
-                && Objects.equals(this.ruleset_id, rule.ruleset_id)
+                && Objects.equals(this.ruleset, rule.ruleset)
                 && Objects.equals(this.priority, rule.priority)
                 && Objects.equals(this.event_type, rule.event_type);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(this.id, this.ruleset_id, this.priority, this.event_type);
+        return Objects.hash(this.id, this.ruleset, this.priority, this.event_type);
     }
 
     @Override
     public String toString() {
-        return "Rule{" + "id=" + this.id + ", ruleset_id=" + this.ruleset_id + ", priority=" + this.priority + ", event_type=" + this.event_type;
+        return "Rule{" + "id=" + this.id + ", ruleset_id=" + this.ruleset + ", priority=" + this.priority + ", event_type=" + this.event_type;
     }
 }
