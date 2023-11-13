ffbe7fcd3b6574a0e63f12d42c7d50b2508f7252
Zach Duncan <izachary.duncan@gmail.com>
2023-11-13 13:26:07 -0600

    finished tests for sidebar. Should be FINAL

--- front-end/src/app/sidebar/sidebar.component.spec.ts
+++ front-end/src/app/sidebar/sidebar.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SidebarComponent } from './sidebar.component';
+import { By } from '@angular/platform-browser';
 
 describe('SidebarComponent', () => {
   let component: SidebarComponent;
@@ -18,4 +19,31 @@ describe('SidebarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('sidebar should be rendered', () => {
+    fixture.detectChanges();
+    const rendered = fixture.debugElement.nativeElement.querySelector('#sidebar-title');
+    expect(rendered.innerHTML).toBe('Super Cool Name');
+  });
+  describe('Should render all sidebar text', () => {
+    it('should render "Dashboard"', () => {
+      const dashboard = fixture.debugElement.query(By.css('#dashboard')).nativeElement;
+      expect(dashboard.innerHTML).toBe('Dashboard');
+    });
+    it('should render "Reports"', () => {
+      const reports = fixture.debugElement.query(By.css('#reports')).nativeElement;
+      expect(reports.innerHTML).toBe('Reports');
+  
+    });
+    it('should render "Simulation"', () => {
+      const simulation = fixture.debugElement.query(By.css('#simulation')).nativeElement;
+      expect(simulation.innerHTML).toBe('Simulation');
+  
+    });
+    it('should render "Sign Out"', () => {
+      const signOut = fixture.debugElement.query(By.css('#signOut')).nativeElement;
+      
+      expect(signOut.innerHTML).toBe('Sign Out');
+    });
+  });
+  
 });
