0fcf88df4581dfecc68a13c310df431f168dad1b
unknown <sophialhales@gmail.com>
2023-11-15 11:22:07 -0600

    Added all components as dependencies in app, added all rules components as dependencies in rulesComponent, added rules as provider for move-up component, added rules as a provider for create-new-condition component, added testing for create-new-condition component, cleaned up code comments

--- front-end/src/app/rules-component/rules-component.component.spec.ts
+++ front-end/src/app/rules-component/rules-component.component.spec.ts
@@ -1,6 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ThenComponent } from '../then/then.component';
+import { ConditionsComponent } from '../conditions/conditions.component';
+import { WhenComponent } from '../conditions/when/when.component';
+import { IsComponent } from '../conditions/is/is.component';
+import { CreateNewConditionComponent } from '../create-new-condition/create-new-condition.component';
+import { DeleteRuleButtonComponent } from '../delete-rule-button/delete-rule-button.component';
+import { MoveDownButtonComponent } from '../move-down-button/move-down-button.component';
+import { MoveUpButtonComponent } from '../move-up-button/move-up-button.component';
+import { By } from '@angular/platform-browser';
 
 import { RulesComponentComponent } from './rules-component.component';
+import { AppComponent } from '../app.component';
 
 describe('RulesComponentComponent', () => {
   let component: RulesComponentComponent;
@@ -8,7 +18,7 @@ describe('RulesComponentComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [RulesComponentComponent]
+      declarations: [RulesComponentComponent, ThenComponent, ConditionsComponent, WhenComponent, IsComponent, CreateNewConditionComponent, DeleteRuleButtonComponent, MoveDownButtonComponent, MoveUpButtonComponent],
     });
     fixture = TestBed.createComponent(RulesComponentComponent);
     component = fixture.componentInstance;
@@ -18,4 +28,15 @@ describe('RulesComponentComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('When "Add Condition" button is clicked, new condition component is rendered on screen', () => {
+    let conditionComponentsArray = fixture.debugElement.nativeElement.querySelectorAll('.conditionsComponent');
+    expect(conditionComponentsArray.length).toBe(1);
+    let addConditionButton = fixture.debugElement.query(By.css('.addConditionButton')).nativeElement;
+    addConditionButton.click();
+    fixture.detectChanges();
+    conditionComponentsArray = fixture.debugElement.nativeElement.querySelectorAll('.conditionsComponent');
+    expect(conditionComponentsArray.length).toBe(2);
+  });
+
 });
