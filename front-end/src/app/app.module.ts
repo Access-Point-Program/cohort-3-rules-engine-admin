5255926898cc02ca3ea57db503172abde2f66506
Sergio Rodas <rodaserg0208Agmail.com>
2023-11-20 11:28:58 -0600

    created cancel button component

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { RulesetNameComponent } from './ruleset-name/ruleset-name.component';
 import { SaveButtonComponent } from './save-button/save-button.component';
 import { DashboardPathComponent } from './dashboard-path/dashboard-path.component';
 import { CreateRulesetComponent } from './create-ruleset/create-ruleset.component';
+import { CancelButtonComponent } from './cancel-button/cancel-button.component';
 
 @NgModule({
   declarations: [
@@ -34,7 +35,8 @@ import { CreateRulesetComponent } from './create-ruleset/create-ruleset.componen
     RulesetNameComponent,
     SaveButtonComponent,
     DashboardPathComponent,
-    CreateRulesetComponent
+    CreateRulesetComponent,
+    CancelButtonComponent
   ],
   imports: [
     BrowserModule    
