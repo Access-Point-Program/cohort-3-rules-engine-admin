3f2eed27c5ab43928ae2791f62949ddb1d76a9aa
llXGamerll <96928244+llXGamerll@users.noreply.github.com>
2023-11-30 10:17:37 -0600

    Updated Save button test to remove ghost code

--- front-end/src/app/save-button/save-button.component.spec.ts
+++ front-end/src/app/save-button/save-button.component.spec.ts
@@ -1,37 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SaveButtonComponent } from './save-button.component';
 import { By } from '@angular/platform-browser';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { HttpClient } from '@angular/common/http';
 import { CreateRulesetComponent } from '../create-ruleset/create-ruleset.component';
-import { Data } from '@angular/router';
 
 
 describe('SaveButtonComponent', () => {
   let component: SaveButtonComponent;
   let fixture: ComponentFixture<SaveButtonComponent>;
   let httpClient: HttpClient;
-  let httpTestingController: HttpTestingController;
-
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [SaveButtonComponent],
-      imports: [ HttpClientTestingModule ],
       providers: [ CreateRulesetComponent ]
     });
 
     httpClient = TestBed.inject(HttpClient);
-    httpTestingController = TestBed.inject(HttpTestingController);
 
     fixture = TestBed.createComponent(SaveButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
     
   });
-
-  // afterEach(() => {
-  //   httpTestingController.verify();
-  // });
 
   it('should create', () => {
     expect(component).toBeTruthy();
