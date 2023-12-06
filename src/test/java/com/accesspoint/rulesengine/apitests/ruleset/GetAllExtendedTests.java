ecd59a36fdf6789f1a84e123c188d83cfbec57b4
unknown <sophialhales@gmail.com>
2023-12-06 09:51:38 -0600

    removed import

--- src/test/java/com/accesspoint/rulesengine/apitests/ruleset/GetAllExtendedTests.java
+++ src/test/java/com/accesspoint/rulesengine/apitests/ruleset/GetAllExtendedTests.java
@@ -7,7 +7,6 @@ import io.restassured.http.ContentType;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
