d231198dfe0288ced810a658dc4d109941ef7889
llXGamerll <96928244+llXGamerll@users.noreply.github.com>
2023-12-06 13:41:14 -0600

    updated confirmation popup message

--- front-end/src/app/sidebar/sidebar.component.ts
+++ front-end/src/app/sidebar/sidebar.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class SidebarComponent {
   leavePage(url: string){
-    if (confirm("Are you sure you want to leave?\nAny changes won't be saved!")) {
+    if (confirm("Are you sure you want to leave?\nAll changes will be lost :(")) {
       window.location.href = url;  
     }
   }
