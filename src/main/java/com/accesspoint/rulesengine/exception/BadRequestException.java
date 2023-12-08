eb6d5b75077a90755c43b5dd4395b0a8f1b8ff20
Sergio Rodas <rodaserg0208Agmail.com>
2023-12-08 11:24:36 -0600

    backend stuff

--- src/main/java/com/accesspoint/rulesengine/exception/BadRequestException.java
+++ src/main/java/com/accesspoint/rulesengine/exception/BadRequestException.java
@@ -1,6 +1,5 @@
 package com.accesspoint.rulesengine.exception;
 
-
 public class BadRequestException extends RuntimeException{
     public BadRequestException(String message) {
         super(message);
--- src/test/java/com/accesspoint/rulesengine/apitests/ruleset/DeleteTests.java
+++ src/test/java/com/accesspoint/rulesengine/apitests/ruleset/DeleteTests.java
@@ -14,13 +14,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.TimeZone;
-
 import static com.accesspoint.rulesengine.entity.EventType.FORWARD;
 import static com.accesspoint.rulesengine.entity.FactType.FRONT;
 import static com.accesspoint.rulesengine.entity.FactType.LEFT;
